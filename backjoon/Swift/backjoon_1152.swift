ff82b5273261ddf60a9b6654f6362cbd3dab904d
Jae-eun <je4297@naver.com>
2019-06-11 20:04:30 +0900

    20190611_#1

---
+++ backjoon/Swift/backjoon_1152.swift
@@ -0,0 +1,16 @@
+//
+//  backjoon_1152.swift
+//  algo
+//
+//  Created by 이재은 on 11/06/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+// backjoon 1152 단어의 개수
+// 주어진 문자열의 단어의 개수 출력
+// 문자열의 길이는 1,000,000을 넘지 않는다. 단어는 띄어쓰기 한 개로 구분되며, 공백이 연속해서 나오는 경우는 없다.
+
+let input = readLine()!.split(separator: " ")
+print(input.count)
