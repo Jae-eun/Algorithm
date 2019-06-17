c99ff9ae3c40f057e244ade5e6e5a27bb8119c12
Jae-eun <je4297@naver.com>
2019-06-18 02:18:42 +0900

    20190618_#1

--- backjoon/Swift/backjoon_2747.swift
+++ backjoon/Swift/backjoon_2747.swift
@@ -12,6 +12,7 @@ import Foundation
 // n이 주어졌을 때, n번째 피보나치 수를 구하기
 // n은 45보다 작거나 같은 자연수
 
+// 풀이 1
 let n = Int(readLine()!)!
 var fibonacci = [Int](repeating: 0, count: n+1)
 fibonacci[1] = 1
@@ -20,3 +21,18 @@ for i in 1..<n {
     fibonacci[i+1] = fibonacci[i-1] + fibonacci[i]
 }
 print(fibonacci[n])
+
+
+// 풀이 2
+let input = Int(readLine()!)!
+var fibonacciArray: [Int] = [0,1]
+
+func fibonacci(_ n: Int) -> Int {
+    if (fibonacciArray.count > n) {
+        return fibonacciArray[n]
+    }
+    let fibonacciNumber = fibonacci(n - 1) + fibonacci(n - 2)
+    fibonacciArray.append(fibonacciNumber)
+    return fibonacciNumber
+}
+print(fibonacci(input))
