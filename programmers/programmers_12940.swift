0ab32e80e3d55262bd261e29872f3d5da47d5f6b
Jae-eun <je4297@naver.com>
2019-06-27 00:33:40 +0900

    20190627_#1

--- programmers/programmers_12940.swift
+++ programmers/programmers_12940.swift
@@ -12,6 +12,7 @@ import Foundation
 // 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환
 // 두 수는 1이상 1000000이하의 자연수
 
+// 풀이 1
 func gcd( _ a: Int, _ b: Int) -> Int {
     if b == 0 {
         return a
@@ -34,5 +35,20 @@ func solution(_ n:Int, _ m:Int) -> [Int] {
     result.append(lcm(n, m))
     return result
 }
+
+// 풀이 2
+func gcd( _ a: Int, _ b: Int) -> Int {
+    let mod = a % b
+    return mod == 0 ? min(a, b) : gcd(b, mod)
+}
+
+func lcm(_ m: Int, _ n: Int) -> Int {
+    return m * n / gcd(m, n)
+}
+
+func solution(_ n:Int, _ m:Int) -> [Int] {
+    return [gcd(n, m), lcm(n, m)]
+}
+
 
 print(solution(3, 12))
