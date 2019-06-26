0a05d44c205d878b47ff3230a111210dafc87b7f
Jae-eun <je4297@naver.com>
2019-06-27 00:19:13 +0900

    20190627_#1

---
+++ programmers/programmers_12940.swift
@@ -0,0 +1,38 @@
+//
+//  programmers_12940.swift
+//  algo
+//
+//  Created by 이재은 on 27/06/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+// programmers 12940 최대공약수와 최소공배수
+// 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환
+// 두 수는 1이상 1000000이하의 자연수
+
+func gcd( _ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        if a > b {
+            return gcd(a-b, b)
+        } else {
+            return gcd(a, b-a)
+        }
+    }
+}
+
+func lcm(_ m: Int, _ n: Int) -> Int {
+    return m * n / gcd(m, n)
+}
+
+func solution(_ n:Int, _ m:Int) -> [Int] {
+    var result = [Int]()
+    result.append(gcd(n, m))
+    result.append(lcm(n, m))
+    return result
+}
+
+print(solution(3, 12))
