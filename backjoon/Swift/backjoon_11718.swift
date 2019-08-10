d55cdd3c0ae4300dccf57fa44ad9583419454abb
Jae-eun <je4297@naver.com>
2019-08-10 17:13:02 +0900

    20190810_#1

---
+++ backjoon/Swift/backjoon_11718.swift
@@ -0,0 +1,18 @@
+//
+//  backjoon_11718.swift
+//  algo
+//
+//  Created by 이재은 on 10/08/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+// backjoon 11718 그대로 출력하기
+// 입력 받은 대로 출력
+//
+// 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다. 각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다. 또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
+
+while let input = readLine() {
+    print(input)
+}
