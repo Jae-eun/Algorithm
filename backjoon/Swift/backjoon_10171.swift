20064452d3a5141bce15727db412117f43ad138e
Jae-eun <je4297@naver.com>
2019-09-04 23:09:21 +0900

    20190904_#4

---
+++ backjoon/Swift/backjoon_10171.swift
@@ -0,0 +1,23 @@
+//
+//  backjoon_10171.swift
+//  algo
+//
+//  Created by 이재은 on 04/09/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+// backjoon 10171 고양이
+// 이 아래 예제와 같이 고양이를 출력하시오.
+//
+// 입력
+// 없음.
+//
+// 출력
+// 고양이를 출력한다.
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
