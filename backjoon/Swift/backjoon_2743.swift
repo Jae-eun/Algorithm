9f21d1557191801968a1db2487803318f4fde4fe
Jae-eun <je4297@naver.com>
2019-09-18 01:35:39 +0900

    20190918_#1

---
+++ backjoon/Swift/backjoon_2743.swift
@@ -0,0 +1,24 @@
+//
+//  backjoon_2743.swift
+//  algo
+//
+//  Created by 이재은 on 18/09/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+//backjoon 단어 길이 재기 2743
+//
+//알파벳으로만 이루어진 단어를 입력받아, 그 길이를 출력하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 영어 소문자와 대문자로만 이루어진 단어가 주어진다. 단어의 길이는 최대 100이다.
+//
+//출력
+//첫째 줄에 입력으로 주어진 단어의 길이를 출력한다.
+
+print(readLine()!.count)
+
+//pulljima
+//답 8
