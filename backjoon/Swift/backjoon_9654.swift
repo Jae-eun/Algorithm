4769d73e1ac1adf96226ff3a91266b3a2f739627
Jae-eun <je4297@naver.com>
2019-10-24 04:20:42 +0900

    20191024_#8

---
+++ backjoon/Swift/backjoon_9654.swift
@@ -0,0 +1,36 @@
+//
+//  backjoon_9654.swift
+//  algo
+//
+//  Created by 이재은 on 24/10/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+//backjoon 나부 함대 데이터 9654
+//
+//나부 행성의 함대 정보를 출력하는 프로그램을 작성하시오.
+//
+//출력
+//나부 행성의 함대의 정보를 아래와 예제 출력과 같은 표로 출력한다.
+//
+//처음 두 열의 너비는 문자 15개, 세 번째 열은 11개, 마지막 열의 너비는 10개이다.
+
+let space = "        "
+print("""
+    SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+    N2 Bomber      Heavy Fighter  Limited    21\(space)
+    J-Type 327     Light Combat   Unlimited  1\(space)
+    NX Cruiser     Medium Fighter Limited    18\(space)
+    N1 Starfighter Medium Fighter Unlimited  25\(space)
+    Royal Cruiser  Light Combat   Limited    4\(space)
+    """)
+
+//SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+//N2 Bomber      Heavy Fighter  Limited    21
+//J-Type 327     Light Combat   Unlimited  1
+//NX Cruiser     Medium Fighter Limited    18
+//N1 Starfighter Medium Fighter Unlimited  25
+//Royal Cruiser  Light Combat   Limited    4
+
