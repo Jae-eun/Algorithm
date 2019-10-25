c9f5c14f043c16115f54b693b36d807bf09ac3b5
Jae-eun <je4297@naver.com>
2019-10-25 22:24:06 +0900

    20191025_#4

---
+++ leetcode/53. Maximum Subarray LeetCode.swift
@@ -0,0 +1,29 @@
+//
+//  53. Maximum Subarray LeetCode.swift
+//  algo
+//
+//  Created by 이재은 on 25/10/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+//53. Maximum Subarray LeetCode
+//
+//Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+func maxSubArray(_ nums: [Int]) -> Int {
+    var sum = [Int](repeating: 0, count: nums.count)
+    sum[0] = nums[0]
+    for i in 1..<nums.count {
+        sum[i] = max(sum[i-1] + nums[i], nums[i])
+    }
+    return sum.max()!
+}
+
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) // 6
+print(maxSubArray([1])) // 1
+
+//Input: [-2,1,-3,4,-1,2,1,-5,4],
+//Output: 6
+//Explanation: [4,-1,2,1] has the largest sum = 6.
