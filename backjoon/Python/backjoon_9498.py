3afc2a489ad771a70e23d9898d23965f315e1ec2
Jae-eun <je6752@gmail.com>
2020-06-06 21:16:47 +0900

    20200606_#9

--- backjoon/Python/backjoon_9498.py
+++ backjoon/Python/backjoon_9498.py
@@ -11,13 +11,8 @@
 
 score = int(input())
 
-if score <= 59:
-    print("F")
-elif score <= 69:
-    print("D")
-elif score <= 79:
-    print("C")
-elif score <= 89:
-    print("B")
-else:
-    print("A")
+if score >= 90: print("A")
+elif score >= 80: print("B")
+elif score >= 70: print("C")
+elif score >= 60: print("D")
+else: print("F")
