3dc7fa5981ab95318a3fea5150191ee9900f1d3b
Jae-eun <je6752@gmail.com>
2020-10-28 23:40:52 +0900

    20201028_#1

--- backjoon/Swift/backjoon_2675.swift
+++ backjoon/Swift/backjoon_2675.swift
@@ -8,18 +8,20 @@
 
 import Foundation
 
-// backjoon 문자열 반복 2675
+//backjoon 문자열 반복 2675
 
-// 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+//문제
+//문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
 //
-// QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
+//QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
 //
-// 입력
-// 첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
+//입력
+//첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
 //
-// 출력
-// 각 테스트 케이스에 대해 P를 출력한다.
+//출력
+//각 테스트 케이스에 대해 P를 출력한다.
 
+// 풀이 1
 let n = Int(readLine()!)!
 
 for _ in 0..<n {
@@ -29,10 +31,18 @@ for _ in 0..<n {
     print(text)
 }
 
+// 풀이 2
+for _ in 0..<Int(readLine()!)! {
+    let input = readLine()!.split(separator: " ")
+    input[1].forEach {
+        print(String(repeating: $0, count: Int(input[0])!), terminator: "")
+    }
+    print()
+}
 
 //2
 //3 ABC
 //5 /HTP
-//
+//답
 //AAABBBCCC
-/////HHHHHTTTTTPPPPP
+///////HHHHHTTTTTPPPPP
