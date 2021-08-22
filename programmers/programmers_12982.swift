d972f0319d0a036f9d34e7321eb767b53643890b
Jae-eun <je6752@gmail.com>
2021-08-23 00:48:34 +0900

    20210823_#1_예산

--- programmers/programmers_12982.swift
+++ programmers/programmers_12982.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 // programmers 12982 예산
+
 // S사에서는 각 부서에 필요한 물품을 지원해 주기 위해 부서별로 물품을 구매하는데 필요한 금액을 조사했습니다. 그러나, 전체 예산이 정해져 있기 때문에 모든 부서의 물품을 구매해 줄 수는 없습니다. 그래서 최대한 많은 부서의 물품을 구매해 줄 수 있도록 하려고 합니다.
 //
 // 물품을 구매해 줄 때는 각 부서가 신청한 금액만큼을 모두 지원해 줘야 합니다. 예를 들어 1,000원을 신청한 부서에는 정확히 1,000원을 지원해야 하며, 1,000원보다 적은 금액을 지원해 줄 수는 없습니다.
@@ -20,23 +21,21 @@ import Foundation
 // d의 각 원소는 부서별로 신청한 금액을 나타내며, 부서별 신청 금액은 1 이상 100,000 이하의 자연수입니다.
 // budget은 예산을 나타내며, 1 이상 10,000,000 이하의 자연수입니다.
 
-func solution(_ d:[Int], _ budget:Int) -> Int {
-    let apply = d.sorted()
+func solution(_ d: [Int], _ budget: Int) -> Int {
+    let department = d.sorted()
     var amount = 0
-    var count = 0
-    for money in apply {
+    var departmentCount = 0
+
+    for money in department {
         amount += money
         if amount <= budget {
-            count += 1
+            departmentCount += 1
         } else {
             break
         }
     }
-    return count
+    return departmentCount
 }
 
-print(solution([1,3,2,5,4], 9))
-print(solution([2,2,3,3] , 10))
-
-//[1,3,2,5,4]    9    3
-//[2,2,3,3]    10    4
+print(solution([1,3,2,5,4], 9)) //3
+print(solution([2,2,3,3] , 10)) //4
